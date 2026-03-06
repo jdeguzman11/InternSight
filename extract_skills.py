5057910902dba3652dedc4f33cca0902d2835f38
Justin DeGuzman <justindeguzman@dhcp-10-8-015-144.mobile.reshsg.uci.edu>
2026-03-06 11:04:08 -0800

    Created function that makes a text all lowercase and analyzes skills that are in the SKILLS list to be put into a new list called found_skills.

---
+++ extract_skills.py
@@ -0,0 +1,14 @@
+# Justin DeGuzman
+
+from skills import SKILLS
+
+
+def extract_skills(text):
+    text = text.lower()
+    found_skills = []
+
+    for skill in SKILLS:
+        if skill in text:
+            found_skills.append(skill)
+
+    return found_skills
