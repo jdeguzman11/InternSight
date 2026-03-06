6b35feacab10fd9f98a04921466101111fcbde9a
Justin DeGuzman <justindeguzman@dhcp-10-8-015-144.mobile.reshsg.uci.edu>
2026-03-06 11:00:31 -0800

    Created list of skills to be flagged from job info.

---
+++ skills.py
@@ -0,0 +1,25 @@
+# Justin DeGuzman
+
+SKILLS = [
+    "python",
+    "java",
+    "c++",
+    "javascript",
+    "typescript",
+    "react",
+    "node",
+    "sql",
+    "mongodb",
+    "postgresql",
+    "aws",
+    "docker",
+    "kubernetes",
+    "git",
+    "linux",
+    "pandas",
+    "numpy",
+    "pytorch",
+    "tensorflow",
+    "machine learning",
+    "data science"
+]
